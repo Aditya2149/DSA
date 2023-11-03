a78200a568fe80773e384aec9d26d20be6de3101
Aditya Kumar <aroy86606@gmail.com>
2023-11-03 12:59:06 +0530

    sort 0 1  in the array

---
+++ C++/sort01.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int sort(int arr[],int size){
+    int i=0,j=size-1;
+    while(i<j){
+        if(arr[i]==0)
+        i++;
+        else if(arr[j]==1)
+        j--;
+        else if(arr[i]==1 && arr[j]==0){
+            swap(arr[i],arr[j]);
+            i++;
+            j--;
+        }
+    }
+    return 0;
+}
+int main(){
+    int arr[] = {0,1,0,1,1,0};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    sort(arr,size);
+    for(int i=0;i<size;i++)
+    cout<<arr[i];
+    
+    return 0;
+}
