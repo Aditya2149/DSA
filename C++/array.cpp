4b2070f89cb9d7a992bcb33b1cd725f90c49947c
Aditya Kumar <94384575+Aditya2149@users.noreply.github.com>
2023-12-10 18:00:40 +0530

    array STL done

--- C++/array.cpp
+++ C++/array.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
+#include<array>
+
 // #include<algorithm>
 using namespace std;
 
 int main(){
-    int a[100];
+    // int a[100];
 
-    fill(a, a+10, 5);
+    array<int, 4>a = {1,2,3,4};
 
-    // for(int i=0;i<100;i++){
-    //     a[i] = 1;
+    // fill(a, a+10, 5);
+
+    // for(int i=0;i<a.size();i++){
+    //     cout<<a[i];
     // }
 
-    cout<<a[79]<<endl;
-    cout<<a[0]<<endl;
-    cout<<a[6]<<endl;
-    cout<<a[9]<<endl;
-    cout<<a[10]<<endl;
+    cout<<a.at(2)<<endl;
+    cout<<a.front()<<endl;
+    cout<<a.back()<<endl;
+    // cout<<a[0]<<endl;
+    // cout<<a[6]<<endl;
+    // cout<<a[9]<<endl;
+    // cout<<a[10]<<endl;
 }
